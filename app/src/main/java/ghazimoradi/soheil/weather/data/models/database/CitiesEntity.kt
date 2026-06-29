fe15aa8b57097cc2d4ad3e678a93da6dc7ed10fd
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-29 19:01:11 +0330

    handle room database entity

---
+++ app/src/main/java/ghazimoradi/soheil/weather/data/models/database/CitiesEntity.kt
@@ -0,0 +1,14 @@
+package ghazimoradi.soheil.weather.data.models.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ghazimoradi.soheil.weather.utils.CITIES_TABLE
+
+@Entity(tableName = CITIES_TABLE)
+data class CitiesEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    var name: String? = null,
+    var lat: Double? = null,
+    var lon: Double? = null
+)
