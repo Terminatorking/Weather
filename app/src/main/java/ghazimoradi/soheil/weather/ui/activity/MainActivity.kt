4dd0f7c8757889a1a5ad9ae084a00c0ec525a9fd
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-29 17:43:09 +0330

    refactoring

---
+++ app/src/main/java/ghazimoradi/soheil/weather/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package ghazimoradi.soheil.weather.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ghazimoradi.soheil.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
