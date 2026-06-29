1f4e37b1d26aba7d1cc0948af5b8c163b3e31c3d
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-29 19:00:47 +0330

    handle app constants

---
+++ app/src/main/java/ghazimoradi/soheil/weather/utils/Constants.kt
@@ -0,0 +1,20 @@
+package ghazimoradi.soheil.weather.utils
+
+//Server
+const val BASE_URL = "https://api.openweathermap.org/"
+const val BASE_URL_IMAGE = "https://openweathermap.org/img/wn/"
+const val PNG_IMAGE = "@2x.png"
+const val CONNECTION_TIME = 60L
+
+//Auth
+const val API_KEY = "ec3642e33eaeec7cf611691576ae03e0"
+
+//Database
+const val CITIES_TABLE = "CITIES_TABLE"
+const val CITIES_DATABASE = "CITIES_DATABASE"
+
+//Other
+const val APPID = "appid"
+const val FA = "fa"
+const val METRIC = "metric"
+const val CITIES_LIMIT = 5
