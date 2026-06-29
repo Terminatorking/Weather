6fd7cd57fe784784df19c6985cec028077e4348f
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-29 17:39:56 +0330

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,97 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.androidx.navigation.safeargs)
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "ghazimoradi.soheil.weather"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "ghazimoradi.soheil.weather"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+
+    //fragment ktx
+    implementation(libs.androidx.fragment.ktx)
+
+    //Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //Dagger - Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //OkHTTP client
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    //Room components
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    //Image Loading
+    implementation(libs.coil)
+    implementation(libs.coil.network.okhttp)
+
+    //Gson
+    implementation(libs.gson)
+
+    //Calligraphy
+    implementation(libs.calligraphy3)
+    implementation(libs.viewpump)
+
+    // Location services
+    implementation(libs.location.services)
+
+    //Other
+    implementation(libs.dynamicsizes)
+    implementation(libs.weatherview)
+}
