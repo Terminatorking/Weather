4dd978d2a5dc5870555b937645a604618c5db116
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-06-29 18:47:48 +0330

    init hilt

--- app/src/main/java/ghazimoradi/soheil/weather/ui/activity/MainActivity.kt
+++ app/src/main/java/ghazimoradi/soheil/weather/ui/activity/MainActivity.kt
@@ -2,8 +2,10 @@ package ghazimoradi.soheil.weather.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import ghazimoradi.soheil.weather.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/ghazimoradi/soheil/weather/utils/MyApp.kt
@@ -0,0 +1,7 @@
+package ghazimoradi.soheil.weather.utils
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application()
